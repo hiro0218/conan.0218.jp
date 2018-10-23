361ab065c884e8f5648a7b460ef942dafc5dd7ed
hiro <hiro0218@gmail.com>
2018-10-23 12:55:30 +0900

    import: from conan.0218.jp at present

---
+++ The_truth_of_Raiha-Pass/index.php
@@ -0,0 +1,36 @@
+<?php require_once 'config.php' ?>
+<!doctype html>
+<html lang="ja" class="no-js">
+<head prefix="og: http://ogp.me/ns#  article: http://ogp.me/ns/article#">
+<?php require_once DOCROOT. '/module/meta.php'; ?>
+</head>
+<body>
+    <header>
+        <h1>来葉峠の真実</h1>
+    </header>
+
+<?php require_once DOCROOT. '/module/share.php'; ?>
+
+<?php require_once DOCROOT. '/module/adsense.php'; ?>
+
+<?php require_once DOCROOT. '/module/timeline.php'; ?>
+
+    <footer>
+        <p>Copyright © 2014–2016 <a href="http://b.0218.jp/" target="_blank">hiro</a>. All Rights Reserved.</p>
+    </footer>
+
+    <!--  -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <script src="js/main.js"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-50805440-3', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+</body>
+</html>
